f0907fb0a28e22dc5c64cea0798af1c8f6a21227
ChloeCrozier <chloecrozier@gmail.com>
2024-09-06 11:26:46 -0400

    adding json file of with every pdf link of prev programs

--- scripts/get_all_programs.py
+++ scripts/get_all_programs.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3.10
 import requests
+from bs4 import BeautifulSoup
+import json
 
 url = 'https://open.clemson.edu/comm_programs/'
-validUrls = []
+urlDict = []
 
 id = 1
 response = requests.get(url + str(id))
 while response.status_code == 200:
-    print(f"status code: {response.status_code} for id: {id}")
-    validUrls.append(url + str(id))
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.title.string if soup.title else "No title found"
+    pdfLink = soup.find('a', id='alpha-pdf').get('href') if soup.find('a', id='alpha-pdf') else "No PDF found"
+    print(f"Title: {title} | PDF Link: {pdfLink}")
+    urlDict.append({title: pdfLink})
     id += 1
     response = requests.get(url + str(id))
+
+json.dump(urlDict, open('graduations.json', 'w'))
