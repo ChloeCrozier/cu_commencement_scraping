0c5870192b2783ed9550bb52732b883a82d9584f
ChloeCrozier <chloecrozier@gmail.com>
2024-09-06 10:11:00 -0400

    properly getting the number of programs available

--- scripts/get_all_programs.py
+++ scripts/get_all_programs.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3.10
 import requests
 
 url = 'https://open.clemson.edu/comm_programs/'
+validUrls = []
 
 id = 1
-response = requests.get(url)
-while response.status_code:
-    response = requests.get(url + str(id))
-    print(f"Status Code: %s, ID: %d", response.status_code, id)
+response = requests.get(url + str(id))
+while response.status_code == 200:
+    print(f"status code: {response.status_code} for id: {id}")
+    validUrls.append(url + str(id))
     id += 1
+    response = requests.get(url + str(id))
