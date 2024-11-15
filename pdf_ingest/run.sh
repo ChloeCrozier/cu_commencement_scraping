5e971dd7657ccd1c908f55baa63fb1a252eab66d
ChloeCrozier <chloecrozier@gmail.com>
2024-11-15 10:33:48 -0500

    adding pdf ingest

---
+++ pdf_ingest/run.sh
@@ -0,0 +1,3 @@
+python pdf_ingest.py --path "../data_science_benchmark/course_data/CPSC 4620&6620 Mod 1.pdf" \
+                     --output "../data_science_benchmark/course_data/CPSC 4620&6620 Mod 1.json" \
+                     --mode "json" \
